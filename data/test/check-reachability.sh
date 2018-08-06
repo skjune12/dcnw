2dbd853d34c65026ceb5c39a813a2d83296b6719
Kohei Suzuki <jingle@sfc.wide.ad.jp>
2018-08-06 12:26:48 +0900

    add test script

---
+++ data/test/check-reachability.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+for i in `seq 1 6`
+do
+  for j in `seq 1 6`
+  do
+    for k in `seq 3 6`
+    do
+      cr_num=$((k-2))
+      ip netns exec eg1 ping -c 3 -w 1000 192.168.${k}${j}.2 > /dev/null
+      if [ $? -eq 0 ]; then
+        # echo "ping eg1 to ac${j} via cr${cr_num}: true"
+        ip netns exec eg2 traceroute 192.168.${k}${j}.2
+      fi
+
+      ip netns exec eg2 ping -c 3 -w 1000 192.168.${k}${j}.2 > /dev/null
+      if [ $? -eq 0 ]; then
+        # echo "ping eg1 to ac${j} via cr${cr_num}: true"
+        ip netns exec eg2 traceroute 192.168.${k}${j}.2
+      fi
+
+      ip netns exec ac${i} ping -c 3 -w 1000 192.168.${k}${j}.2 > /dev/null
+      if [ $? -eq 0 ]; then
+        # echo "ping ac${i} to ac${j} via cr${cr_num}: true"
+        ip netns exec ac${i} traceroute 192.168.${k}${j}.2
+      fi
+    done
+  done
+done
